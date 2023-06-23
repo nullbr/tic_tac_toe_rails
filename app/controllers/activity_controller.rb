9180074187fb7c8d9442ddf6d43bdec1a615c380
Bruno Leite <bmarianoleite3@gmail.com>
2023-06-22 21:40:47 -0300

    change styling for activity page

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class ActivityController < ApplicationController
-  before_action :require_signin
-
   def index
     @pagy, @games = pagy(Game.order(created_at: :desc).includes(:players), items: 5)
 
-    @games = current_player.assign_my_likes_to_games(@games)
+    @games = current_player.assign_my_likes_to_games(@games) if current_player
   end
 end
