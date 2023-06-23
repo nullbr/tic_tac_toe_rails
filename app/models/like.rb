681d9f164e6e89e24a1ff2747d9fe27b0939126f
Bruno Leite <bmarianoleite3@gmail.com>
2023-06-23 10:53:07 -0300

    broadcast likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,17 @@
 class Like < ApplicationRecord
+  include ActionView::RecordIdentifier
+
   belongs_to :player
   belongs_to :game, counter_cache: true
+
+  after_create_commit lambda {
+    broadcast_update_later_to 'activity', target: "#{dom_id(game)}_likes_count",
+                                          html: game.likes_count
+  }
+
+  after_destroy_commit lambda {
+    broadcast_update_later_to 'activity', target: "#{dom_id(game)}_likes_count",
+                                          html: game.likes_count,
+                                          locals: { like: nil }
+  }
 end
